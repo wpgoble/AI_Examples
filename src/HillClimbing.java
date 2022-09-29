89782b9fe1f733d05d25bddc2eba2c1599b9c676
William Goble <will.goble@gmail.com>
2022-09-29 11:46:44 -0400

    creates function to set X and Y values randomly

--- src/HillClimbing.java
+++ src/HillClimbing.java
@@ -10,12 +10,24 @@ public class HillClimbing extends JFrame implements Runnable {
     int n = rand.nextInt(300) + 250;
     final int waitTime = 0;                     // milliseconds b/t repaining
 
+    int[] bestState;
+    double[] x = new double[n];
+    double[] y = new double[n];
+
+    public void setXYValues() {
+        for(int i = 0; i < n; i++) {
+            x[i] = rand.nextDouble();
+            y[i] = rand.nextDouble();
+        }
+    }
+
     public void climb() {
         System.out.println("Hello, World");
     }
 
     @Override
     public void run() {
+        setXYValues();
         climb();
     }
 }
