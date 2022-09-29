3683683dffed440a447c8d85ffd00fba9722f846
William Goble <will.goble@gmail.com>
2022-09-29 12:17:32 -0400

    implements a reverse array function and the climb function

--- src/HillClimbing.java
+++ src/HillClimbing.java
@@ -44,14 +44,55 @@ public class HillClimbing extends JFrame implements Runnable {
                 for(int j = i+1; j < n; j++) {
                     int i1 = (i - 1 + n) % n;
                     int j1 = (j + 1) % n;
-                    double delta = dist(x[curState[i1]], y[curState[i1]], 
-                                        x[curState[j]], y[curState[j]]) 
-                                    + dist()
+                    double d_i1j = dist(x[curState[i1]], y[curState[i1]], 
+                                        x[curState[j]], y[curState[j]]);
+                    double d_ij1 = dist(x[curState[i]], y[curState[i]], 
+                                        x[curState[j1]], y[curState[j1]]);
+                    double d_i1i = dist(x[curState[i1]], y[curState[i1]], 
+                                        x[curState[i]], y[curState[i]]);
+                    double d_jj1 = dist(x[curState[j]], y[curState[j]], 
+                                        x[curState[j1]], y[curState[j1]]);
+                    double delta =  d_i1j + d_ij1 - d_i1i - d_jj1;
+
+                    if (delta < bestDelta) {
+                        bestDelta = delta;
+                        best_I = i;
+                        best_J = j;
+                    }
                 }
+            }
+
+            if (bestDelta < -1e-9) {
+                reverse(curState, best_I, best_J);
+                System.arraycopy(curState, 0, bestState, 0, n);
+                repaint();
+
+                try {
+                    Thread.sleep(waitTime);
+                } catch (InterruptedException e) {
+                    // do nothing
+                }
+            } else {
+                done = true;
             }
         }
     }
 
+    // Reverse the order from i to j
+    static void reverse(int[] p, int i, int j) {
+        int n = p.length;
+        while (i != j) {
+            int t = p[j];
+            p[j] = p[i];
+            p[i] = t;
+            i = (i + 1) % n;
+            if (i == j) {
+                break;
+            }
+            j = (j - 1 + n) % n;
+        }        
+    }
+    
     static double dist(double x1, double y1, double x2, double y2) {
         double dx = x1 - x2;
         double dy = y1 - y2;
