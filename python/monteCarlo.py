b6bb186c5df39591fc66db54849fdc97d26e3399
William Goble <will.goble@gmail.com>
2022-09-29 13:22:55 -0400

    adds monte carlo simulation for coin flipping

---
+++ python/monteCarlo.py
@@ -0,0 +1,38 @@
+import random
+import numpy as np
+from matplotlib import pyplot as plt
+
+def coin_flip():
+    """
+    Simulates flipping a coin.
+    0 -> heads
+    1 -> tails
+    """
+    return random.randint(0, 1)
+
+def monteCarlo(n_itr):
+    results = 0
+    lst = list()
+
+    for n in range(n_itr):
+        flip_results = coin_flip()
+        results += flip_results
+
+        # Calculatings the probability value
+        prob_val = results / (n + 1)
+        lst.append(prob_val)
+
+    plt.axhline(y = 0.5, color='r')
+    plt.xlabel("Iterations")
+    plt.ylabel("Probability")
+    plt.plot(lst)
+    plt.show()
+
+    return results / n_itr
+
+def main():
+    answer = monteCarlo(10000)
+    print(f"Final Answer:\t{answer}")
+
+if __name__ == '__main__':
+    main()
