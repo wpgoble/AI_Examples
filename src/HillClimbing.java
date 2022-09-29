8e3602815ed0a809338e2b99ec5c0534df0f0971
William Goble <will.goble@gmail.com>
2022-09-29 11:43:09 -0400

    Overrides run() function

--- src/HillClimbing.java
+++ src/HillClimbing.java
@@ -1,3 +1,21 @@
 // Code is originally from 
 // https://sites.google.com/site/indy256/algo/simulated_annealing
 // FOR DEMO USE ONLY
+
+import javax.swing.*;
+import java.util.Random;
+
+public class HillClimbing extends JFrame implements Runnable {
+    Random rand = new Random();
+    int n = rand.nextInt(300) + 250;
+    final int waitTime = 0;                     // milliseconds b/t repaining
+
+    public void climb() {
+        System.out.println("Hello, World");
+    }
+
+    @Override
+    public void run() {
+        climb();
+    }
+}
