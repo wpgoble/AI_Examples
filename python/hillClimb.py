2f2afb2fd6800f55971db1bc8881c2c51a87a5c4
William Goble <will.goble@gmail.com>
2022-09-30 14:05:14 -0400

    adds second main function which visualizes hill climb against a curve

--- python/hillClimb.py
+++ python/hillClimb.py
@@ -15,36 +15,36 @@ def plot_curve():
     plt.axvline(x = x_optima, ls='--', color = 'red')
     plt.show()
 
-def hillClimbing(objective, bounds, n_itr, step_size):
-    # generate an initial point
-    solution = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
-    # evaluate the initial point
-    solution_eval = objective(solution)
-    # Run the hill climb algorithm
-    scores = list()
-    scores.append(solution_eval)
-
-    for i in range(n_itr):
-        # take a step
-        candidate = solution + randn(len(bounds)) * step_size
-        # evaluate the candidate
-        candidate_eval = objective(candidate)
-        # check if we should keep the new point
-        if candidate_eval <= solution_eval:
-            # store the new point
-            solution, solution_eval = candidate, candidate_eval
-            # keep track of scores
-            scores.append(solution_eval)
-            # report progress
-            print('>%d f(%s) = %.5f' % (i, solution, solution_eval))
-    return [solution, solution_eval, scores]
+# hill climbing local search algorithm
+def hillclimbing(objective, bounds, n_iterations, step_size):
+	# generate an initial point
+	solution = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
+	# evaluate the initial point
+	solution_eval = objective(solution)
+	# run the hill climb
+	solutions = list()
+	solutions.append(solution)
+	for i in range(n_iterations):
+		# take a step
+		candidate = solution + randn(len(bounds)) * step_size
+		# evaluate candidate point
+		candidte_eval = objective(candidate)
+		# check if we should keep the new point
+		if candidte_eval <= solution_eval:
+			# store the new point
+			solution, solution_eval = candidate, candidte_eval
+			# keep track of solutions
+			solutions.append(solution)
+			# report progress
+			print('>%d f(%s) = %.5f' % (i, solution, solution_eval))
+	return [solution, solution_eval, solutions]
 
 def main():
     seed(5)
     bounds = asarray([[-5.0, 5.0]])
     n_iterations = 1000
     step_size = 1e-1
-    best, score, scores = hillClimbing(objective, bounds, n_iterations, step_size)
+    best, score, scores = hillclimbing(objective, bounds, n_iterations, step_size)
     print('Done!')
     print('f(%s) = %f' % (best, score))
 
@@ -53,6 +53,26 @@ def main():
     plt.ylabel('Evaluation f(x)')
     plt.show()
 
+def main2():
+    seed(5)
+    bounds = asarray([[-5.0, 5.0]])
+    n_iterations = 1000
+    step_size = 1e-1
+    best, score, solutions = hillclimbing(objective, bounds, n_iterations, step_size)
+    print('Done!')
+    print('f(%s) = %f' % (best, score))
+    
+    # sample input range uniformly at 0.1 increments
+    inputs = arange(bounds[0,0], bounds[0,1], 0.1)
+    # create a line plot of input vs result
+    plt.plot(inputs, [objective([x]) for x in inputs], '--')
+    # draw a vertical line at the optimal input
+    plt.axvline(x=[0.0], ls='--', color='red')
+    # plot the sample as black circles
+    plt.plot(solutions, [objective(x) for x in solutions], 'o', color='black')
+    plt.show()
+
 if __name__ == '__main__':
-    plot_curve()
+    #plot_curve()
     #main()
+    main2()
